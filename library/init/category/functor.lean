5e67a9e38ced6485954782f3d88f7475805f6592
Sebastian Ullrich <sebasti@nullri.ch>
2017-12-20 18:09:59 +0100

    chore(frontends/lean/notation_cmd): fix `$>` with new def instead of beta-reductions
    This reverts commit d64b216e1b184db3a82b840b00b82bbbeeb4f727.

--- library/init/category/functor.lean
+++ library/init/category/functor.lean
@@ -14,6 +14,9 @@ class has_map (f : Type u → Type v) : Type (max (u+1) v) :=
 
 infixr ` <$> `:100 := has_map.map
 infixr ` <$ `:100  := has_map.map_const
-infixr [parsing_only] ` $> `:100  := λ a b, b <$ a
+
+@[reducible] def has_map.map_const_rev {f : Type u → Type v} [has_map f] {α β : Type u} : f β → α → f α :=
+λ a b, b <$ a
+infixr ` $> `:100  := has_map.map_const_rev
 
 class functor (f : Type u → Type v) extends has_map f
